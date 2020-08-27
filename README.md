d0e77a350cdd35bedc7f396f5fba80ff7db98fa5
Tom Striker <toms.baugis@gmail.com>
2020-08-27 21:44:12 +0100

    v0.1

---
+++ README.md
@@ -0,0 +1,86 @@
+# Web-MIDI wrapper for AKAI Midimix
+
+This package makes it simple to access your AKAI Midimix's state in browser, using the web-midi API.
+
+
+# Install
+
+```npm install akai-midimix```
+
+# Initialization
+```javascript
+import {MidiMix} from "akai-midimix";
+
+const midi = new MidiMix();
+
+
+midi.connect().then(() => {
+    // run some commands right on startup (e.g. setting the toggle lights)
+    console.log("Midi connected!");
+    midi.m1 = true; // will light up the m1 button - see below for layout
+});
+
+midi.addEventListener("cc", (data) => {
+    console.log("CC dial/slider turned", data);
+});
+
+midi.addEventListener("keydown", (data) => {
+    console.log("Button press", data);
+});
+```
+
+# Names for the buttons and sliders
+
+The midi has 8 columns, each column having 3 dials, 2 buttons, and a slider.
+Then, extra, there are the bank left, bank right, solo buttons, and the "master" slider.
+
+To keep things simple, we are making use of the columns. Event data will contain
+both, the original code (in keyCode field) and the symbolic field.
+C stands for "continuous control", "m" for mute, "r" for rec, and "s" for slider.
+
+```
+c1    c2    c3    c4    c5    c6    c7    c8
+c1a   c2a   c3a   c4a   c5a   c6a   c7a   c8a   bank_left
+c1b   c2b   c3b   c4b   c5b   c6b   c7b   c8b   bank_right
+
+m1    m2    m3    m4    m5    m6    m7    m8    solo
+r1    r2    r3    r4    r5    r6    r7    r8
+
+
+s1    s2    s3    s4    s5    s6    s7    s8    master
+```
+
+
+# Reading dial/slider states and setting the button lighs
+
+Note: There seems to be no way to find out the initial state of the knobs when you connect to it. Luckily, that's what
+the "Send All" hardware button is there for - it will generate change events for all the CC controls and the library
+will have their state from there on out. Alternatively, the state for individual controls will be set when you
+physically poke them.
+
+With the caveat above in mind, to get the dial state, simply go `midi.c1` and so on.
+
+All the buttons, with the exception of "Send All" and "Solo" have an LED that you can turn on.
+Simply set the value to true/false accordingly to the button: `midi.bank_left = true`.
+
+
+
+# Events
+
+Use midi instance's `addEventListener(eventType, callback)` and `removeEventListener(eventType, callback)` to
+subscribe to the events. There are 3 events:
+
+* `cc` - fired on slider/dial turn. The event data is `{code, keyCode, val, prevVal}`
+* `keydown` - fired when any of the buttons are pressed (with the exception of send all).
+
+
+# Licence & Thanks
+
+This code is licenced under the MIT license,  so you can do with it whatever you want for whatever purpose.
+
+There is barely any documentation out there.
+
+This link was good to get rolling: https://webmidi-examples.glitch.me/
+
+This google doc has reversed-engineered data on the messages, without which I wouldn't know how to turn the lights on.
+https://docs.google.com/document/d/1zeRPklp_Mo_XzJZUKu2i-p1VgfBoUWF0JKZ5CzX8aB0/
---
+++ index.js
@@ -0,0 +1,233 @@
+class MidiMix {
+    constructor(options) {
+        this.connected = false;
+        this._input = null;
+        this._output = null;
+        this._initDone = false;
+
+        this._listeners = [];
+
+        // wrap toggles so that when the value is set, we send the signal to the MIDI light
+        let _states = {};
+        Object.keys(MidiButtons).forEach(button => {
+            if (button == "solo") {
+                // solo doesn't have a light
+                return;
+            }
+            Object.defineProperty(this, button, {
+                get() {
+                    return _states[button] || false;
+                },
+                set: bool => {
+                    if (bool == _states[button]) {
+                        return;
+                    }
+
+                    if (!this.connected) {
+                        throw new Error("MIDI not connected!");
+                    }
+                    _states[button] = bool;
+                    this._output.send([0x90, MidiButtons[button], bool ? 127 : 0]);
+
+                    // when the light gets toggled, we pretend there has been a cc event as we have a state for them
+                    if (this._initDone) {
+                        this._dispatchEvent("cc", {
+                            code: button,
+                            keyCode: MidiButtons[button],
+                            val: bool,
+                            prevVal: !bool,
+                        });
+                    }
+                },
+            });
+        });
+
+        this._onMessage = this._onMessage.bind(this);
+        this._onStateChange = this._onStateChange.bind(this);
+    }
+
+    async connect() {
+        return new Promise(async resolve => {
+            let access = await navigator.requestMIDIAccess({sysex: true});
+            // MIDI devices that send you data.
+            const inputs = access.inputs.values();
+            for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+                if (input.value.name.indexOf("MIDI Mix") == 0) {
+                    this._input = input.value;
+                }
+            }
+
+            if (!this._input) {
+                console.error("Tried to connect to MIDI Mix but didn't find one.");
+                return;
+            }
+            this._input.addEventListener("midimessage", this._onMessage);
+            this._input.addEventListener("statechange", this._onStateChange);
+
+            const outputs = access.outputs.values();
+            for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
+                if (output.value.name.indexOf("MIDI Mix") == 0) {
+                    this._output = output.value;
+                }
+            }
+
+            let tempListener = evt => {
+                if (evt.port.state == "connected") {
+                    this._input.removeEventListener("statechange", tempListener);
+
+                    Object.keys(MidiButtons).forEach(button => {
+                        // reset the buttons lights on load as we can't read their state
+                        this[button] = false;
+                    });
+                    this._initDone = true;
+                    resolve();
+                }
+            };
+            this._input.addEventListener("statechange", tempListener);
+        });
+    }
+
+    addEventListener(eventType, listener) {
+        this._listeners.push([eventType, listener]);
+    }
+
+    removeEventListener(eventType, listener) {
+        let idx = -1;
+        this._listeners.forEach(([lType, lFunc], idx) => {
+            if (lType == eventType && lFunc == listener) {
+                idx = idx;
+            }
+        });
+        if (idx != -1) {
+            this._listeners.splice(idx, 1);
+        }
+    }
+
+    _dispatchEvent(eventType, data) {
+        /* emits a custom event with cc data */
+        this._listeners.forEach(([listenerType, listener]) => {
+            if (listenerType == eventType) {
+                listener({...data, type: eventType});
+            }
+        });
+    }
+
+    _onMessage(event) {
+        if (event.data.lengt > 3) {
+            this._handleSysexMessage(event);
+            return;
+        }
+
+        let [typeId, knob, val] = event.data;
+        let eventType = {144: "note_on", 128: "note_off", 176: "cc"}[typeId];
+        let code = eventType == "cc" ? ccMapping[knob] : buttonMapping[knob];
+
+        if (eventType == "cc") {
+            // normalize the value and round to the 6th digit as that's far enough
+            let prev = this[code];
+            this[code] = round(val / 127, 6);
+            this._dispatchEvent("cc", {code: code, keyCode: knob, val: this[code], prevVal: prev});
+        } else {
+            // button press
+            let pascalCase = {bank_left: "BankLeft", bank_right: "BankRight", solo: "Solo"};
+            this._dispatchEvent(eventType == "note_on" ? "keydown" : "keyup", {
+                key: code,
+                code: pascalCase[code] || code,
+                keyCode: knob,
+            });
+        }
+    }
+
+    _handleSysexMessage(event) {
+        // sysex event (e.g. reading conf) not used right now
+        let [_start, _manufacturerID, _deviceID, _modelId, messageId, _msb, _lsb, ...data] = [...event.data];
+        console.log("sysex message received:", messageId, event, data.join(","));
+    }
+
+    _onStateChange(event) {
+        this.connected = event.port.state == "connected";
+    }
+
+    disconnect() {
+        if (this._input) {
+            this._input.removeEventListener("midimessage", this._onMessage);
+            this._input.removeEventListener("statechange", this._onStateChange);
+            this._input = null;
+        }
+        this._output = null;
+    }
+
+    destroy() {
+        this.disconnect();
+        this._watchers = [];
+    }
+}
+
+function round(val, precision = 0) {
+    // rounds the number to requested precision. how is this not part of stdlib
+    return Math.round(val * Math.pow(10, precision)) / Math.pow(10, precision);
+}
+
+let MidiCc = {
+    c1: 16,
+    c2: 20,
+    c3: 24,
+    c4: 28,
+    c5: 46,
+    c6: 50,
+    c7: 54,
+    c8: 58,
+    c1a: 17,
+    c2a: 21,
+    c3a: 25,
+    c4a: 29,
+    c5a: 47,
+    c6a: 51,
+    c7a: 55,
+    c8a: 59,
+    c1b: 18,
+    c2b: 22,
+    c3b: 26,
+    c4b: 30,
+    c5b: 34,
+    c6b: 53,
+    c7b: 56,
+    c8b: 60,
+
+    s1: 19,
+    s2: 23,
+    s3: 27,
+    s4: 31,
+    s5: 49,
+    s6: 53,
+    s7: 57,
+    s8: 61,
+    master: 62,
+};
+let ccMapping = Object.fromEntries(Object.entries(MidiCc).map(([key, val]) => [val, key]));
+
+let MidiButtons = {
+    m1: 1,
+    m2: 4,
+    m3: 7,
+    m4: 10,
+    m5: 13,
+    m6: 16,
+    m7: 19,
+    m8: 22,
+    r1: 3,
+    r2: 6,
+    r3: 9,
+    r4: 12,
+    r5: 15,
+    r6: 18,
+    r7: 21,
+    r8: 24,
+    bank_left: 25,
+    bank_right: 26,
+    solo: 27,
+};
+let buttonMapping = Object.fromEntries(Object.entries(MidiButtons).map(([key, val]) => [val, key]));
+
+export {MidiMix, MidiCc, MidiButtons};
+export default MidiMix;
